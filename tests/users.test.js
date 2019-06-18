c656f6be2c843a7867453da72e7d649776fbe386
aaron shivers <aaorn@moonmail.org>
2019-06-17 20:09:44 -0500

    refactored tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -50,236 +50,395 @@ describe('/users', () => {
 
   // POST /users
   describe('POST /users', () => {
-    
-    it('should respond 400, and NOT create user, if email is invalid', async () => {
 
-      const user = { email: 'asdf', password: 'asdfASDF1234!@#$' }
+    describe('when `email` is invalid', () => {
 
-      await request(app)
-        .post('/users')
-        .send(user)
-        .expect(400)
+      it('should respond 400', async () => {
+
+        const user = { email: 'asdf', password: 'asdfASDF1234!@#$' }
+
+        await request(app)
+          .post('/users')
+          .send(user)
+          .expect(400)
+      })
+
+      it('should NOT create user', async () => {
+
+        const user = { email: 'asdf', password: 'asdfASDF1234!@#$' }
+
+        await request(app)
+          .post('/users')
+          .send(user)
+
+        const foundUsers = await User.find()
+        expect(foundUsers.length).toBe(2)
+      })
     })
-
-    it('should respond 400, and NOT create user, if password is invalid', async () => {
+    
+    describe('when `password` is invalid', () => {
 
       const user = { email: 'user2@test.net', password: 'pass', token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET) }
 
-      await request(app)
-        .post('/users')
-        .send(user)
-        .expect(400)
+      it('should respond 400', async () => {
+
+        await request(app)
+          .post('/users')
+          .send(user)
+          expect(400)
+      })
+
+      it('should NOT create user', async () => {
+
+        await request(app)
+          .post('/users')
+          .send(user)
+
+        const foundUsers = await User.find()
+        expect(foundUsers.length).toBe(2)
+      })
     })
 
-    it('should respond 400, and NOT create user, if user already exists', async () => {
-      
-      const user = userZero
+    describe('when `user` already exists', () => {
 
-      await request(app)
-        .post('/users')
-        .send(user)
-        .expect(400)
+      it('should respond 400', async () => {
+        
+        const user = userZero
+
+        await request(app)
+          .post('/users')
+          .send(user)
+      })
+
+      it('should NOT create user', async () => {
+        
+        const user = userZero
+
+        await request(app)
+          .post('/users')
+          .send(user)
+
+        const foundUsers = await User.find()
+        expect(foundUsers.length).toBe(2)
+      })
     })
-    
-    it(`should respond 200, hash password, create and return new user and token`, async () => {
+
+    describe('when data is valid', () => {
 
       const user = { email: 'user2@test.net', name: 'user 2', password: 'asdfASDF1234!@#$' }
 
-      await request(app)
-        .post('/users')
-        .send(user)
-        .expect(200)
-        .expect(res => {
-          expect(res.body.user.email).toEqual(user.email)
-          expect(res.body.user.password).not.toEqual(user.password)
-          expect(res.body.token).toBeTruthy()
-        })
+      it(`should respond 200`, async () => {
 
-      const foundUser = await User.findOne({ email: user.email })
-      expect(foundUser).toBeTruthy()
-      expect(foundUser.email).toEqual(user.email)
-      expect(foundUser.password).not.toEqual(user.password)
+        await request(app)
+          .post('/users')
+          .send(user)
+          .expect(200)
+      })
+
+      it(`should hash the password`, async () => {
+
+        await request(app)
+          .post('/users')
+          .send(user)
+          .expect(res => {
+            expect(res.body.user.password).not.toEqual(user.password)
+          })
+
+        const foundUser = await User.findOne({ email: user.email })
+        expect(foundUser.password).not.toEqual(user.password)
+      })
+
+      it(`should create and return new user and token`, async () => {
+
+        await request(app)
+          .post('/users')
+          .send(user)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.user.email).toEqual(user.email)
+            expect(res.body.token).toBeTruthy()
+          })
+
+        const foundUser = await User.findOne({ email: user.email })
+        expect(foundUser).toBeTruthy()
+        expect(foundUser.email).toEqual(user.email)
+      })
     })
   })
 
   // GET /users
   describe('GET /users', () => {
 
-    it('should respond 401 if token is not provided', async () => {
+    describe('when no `token` is provided', () => {
 
-      await request(app)
-        .get(`/users`)
-        .expect(401)
+      it('should respond 401', async () => {
+
+        await request(app)
+          .get(`/users`)
+          .expect(401)
+      })
     })
 
-    it('should respond 401 if user is NOT admin', async () => {
+    describe('when a `token` is provided', () => {
 
-      await request(app)
-        .get('/users')
-        .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
-        .expect(401)
-    })
+      describe('and `user` is not an admin', () => {
 
-    it('should get all users if user is admin', async () => {
+        it('should respond 401', async () => {
 
-      await request(app)
-        .get('/users')
-        .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
-        .expect(200)
-        .expect((res) => {
-          expect(res.text).toContain(userZero.email)
-          expect(res.text).toContain(userOne.email)
+          await request(app)
+            .get('/users')
+            .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
+            .expect(401)
         })
+      })
+
+      describe('and `user` is an admin', () => {
+
+        it('should get all `users`', async () => {
+
+          await request(app)
+            .get('/users')
+            .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+            .expect(200)
+            .expect((res) => {
+              expect(res.text).toContain(userZero.email)
+              expect(res.text).toContain(userOne.email)
+            })
+        })
+      })
     })
   })
 
   // DELETE /users/:id
   describe('DELETE /users/:id', () => {
 
-    it('should respond 401 if token is not provided', async () => {
+    describe('when `token` is not provided', () => {
 
-      await request(app)
-        .delete(`/users/${ userOneId }`)
-        .expect(401)
+      it('should respond 401', async () => {
+
+        await request(app)
+          .delete(`/users/${ userOneId }`)
+          .expect(401)
+      })
     })
 
-    it('should respond 401 if user is NOT an admin', async () => {
+    describe('when `token` is provided', () => {
 
-      await request(app)
-        .delete(`/users/${ userOneId }`)
-        .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
-        .expect(401)
+      describe('and `user` is not an admin', () => {
 
-      const users = await User.find()
-      expect(users.length).toBe(2)
-    })
+        it('should respond 401', async () => {
 
-    it('should return 400 if Id is invalid', async () => {
+          await request(app)
+            .delete(`/users/${ userOneId }`)
+            .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
+            .expect(401)
+        })
+      })
 
-      request(app)
-        .delete(`/users/1234`)
-        .expect(400)
-    })
+      describe('and `user` is an admin', () => {
 
-    it('should return 404 if the specified user is not found', async () => {
+        describe('and `id` is invalid', () => {
 
-      request(app)
-        .delete(`/users/${ new ObjectId() }`)
-        .expect(404)
-    })
+          it('should return 400', async () => {
 
-    it('should delete the user', async () => {
+            request(app)
+              .delete(`/users/1234`)
+              .expect(400)
+          })
+        })
+      })
 
-      await request(app)
-        .delete(`/users/${ userOneId }`)
-        .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
-        .expect(200)
+      describe('and `id` is valid', () => {
 
-      const users = await User.find()
-      expect(users.length).toBe(1)
-      expect(users.toString()).toContain(userZeroId)
+        describe('and `user` is not found', () => {
+
+          it('should return 404', async () => {
+
+            request(app)
+              .delete(`/users/${ new ObjectId() }`)
+              .expect(404)
+          })
+        })
+
+        describe('and `user` is found', () => {
+
+          it('should respond 200', async () => {
+
+            await request(app)
+              .delete(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .expect(200)
+          })
+
+          it('should delete the user', async () => {
+
+            await request(app)
+              .delete(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+
+            const users = await User.find()
+            expect(users.length).toBe(1)
+            expect(users.toString()).toContain(userZeroId)
+          })
+        })
+      })
     })
   })
 
   // PATCH /users/:id
   describe('PATCH /users/:id', () => {
 
-    it('should respond 401 if token is not provided', async () => {
+    describe('when `token` is not provided', () => {
 
-      await request(app)
-        .patch(`/users/${ userOneId }`)
-        .expect(401)
+      it('should respond 401', async () => {
+
+        await request(app)
+          .patch(`/users/${ userOneId }`)
+          .expect(401)
+      })
     })
 
-    it('should respond 401 if user is not an admin', async () => {
+    describe('when `token` is provided', () => {
 
-      const update = {
-        email: 'user@example.net',
-        password: 'asdfASDF1234!@#$',
-        name: 'updated user'
-      }
+      describe('and `user` is not an admin', () => {
 
-      await request(app)
-        .patch(`/users/${ userOneId }`)
-        .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
-        .send(update)
-        .expect(401)
+        it('should respond 401', async () => {
+
+          const update = {
+            email: 'user@example.net',
+            password: 'asdfASDF1234!@#$',
+            name: 'updated user'
+          }
+
+          await request(app)
+            .patch(`/users/${ userOneId }`)
+            .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
+            .send(update)
+            .expect(401)
+        })
+      })
+
+      describe('and `user` is an admin', () => {
+
+        describe('and `ObjectId` is not in the DB', () => {
+
+          it('should respond 404', async () => {
+
+            const update = {
+              email: 'user@example.net',
+              password: 'asdfASDF1234!@#$',
+              name: 'updated user'
+            }
+
+            await request(app)
+              .patch(`/users/${ new ObjectId() }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+              .expect(404)
+          })   
+        })
+      })
+
+      describe('and `ObjectId` is in the DB', () => {
+
+        describe('and `email` is invalid', () => {
+
+            const update = {
+              email: 12341234,
+              password: 'asdfASDF1234!@#$',
+              name: 'updated user'
+            }
+
+          it('should respond 400', async () => {
+
+            await request(app)
+              .patch(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+              .expect(400)
+          })
+
+          it('should NOT update `user`', async () => {
+
+            await request(app)
+              .patch(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+
+            const foundUser = await User.findById(userOneId)
+            expect(foundUser._id).toEqual(userOneId)
+            expect(foundUser.name).toEqual(userOne.name)
+            expect(foundUser.email).not.toEqual(update.email)
+          })
+        })
+
+        describe('and `password` is invalid', () => {
+
+          const update = {
+            email: 'user@example.net',
+            password: 'asdf',
+            name: 'updated user'
+          }
+
+          it('should respond 400', async () => {
+
+            await request(app)
+              .patch(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+              .expect(400)
+          })
+
+          it('should NOT update a user with an invalid password', async () => {
+
+            await request(app)
+              .patch(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+
+            const foundUser = await User.findById(userOneId)
+            expect(foundUser._id).toEqual(userOneId)
+            expect(foundUser.name).toEqual(userOne.name)
+            expect(foundUser.email).not.toEqual(update.email)
+          })
+        })
+
+        describe('and `name` is invaild', () => {
+
+          const update = {
+            email: 'user@example.net',
+            password: 'asfdASDF1234!@#$',
+            name: 'a'
+          }
+
+          it('should respond 400', async () => {
+
+            await request(app)
+              .patch(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+              .expect(400)
+          })
+
+          it('should NOT update a user with an invalid name', async () => {
+            
+            await request(app)
+              .patch(`/users/${ userOneId }`)
+              .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
+              .send(update)
+
+            const foundUser = await User.findById(userOneId)
+            expect(foundUser._id).toEqual(userOneId)
+            expect(foundUser.name).toEqual(userOne.name)
+            expect(foundUser.email).not.toEqual(update.email)
+          })
+        })
+      })
     })
 
-    it('should respond 404 if ObjectId is not in DB', async () => {
 
-      const update = {
-        email: 'user@example.net',
-        password: 'asdfASDF1234!@#$',
-        name: 'updated user'
-      }
 
-      await request(app)
-        .patch(`/users/${ new ObjectId() }`)
-        .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
-        .send(update)
-        .expect(404)
-    })
 
-    it('should NOT update a user with an invalid email', async () => {
-      
-      const update = {
-        email: 12341234,
-        password: 'asdfASDF1234!@#$',
-        name: 'updated user'
-      }
-
-      await request(app)
-        .patch(`/users/${ userOneId }`)
-        .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
-        .send(update)
-        .expect(400)
-
-      const foundUser = await User.findById(userOneId)
-      expect(foundUser._id).toEqual(userOneId)
-      expect(foundUser.name).toEqual(userOne.name)
-      expect(foundUser.email).not.toEqual(update.email)
-    })
-
-    it('should NOT update a user with an invalid password', async () => {
-      
-      const update = {
-        email: 'user@example.net',
-        password: 'asdf',
-        name: 'updated user'
-      }
-
-      await request(app)
-        .patch(`/users/${ userOneId }`)
-        .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
-        .send(update)
-        .expect(400)
-
-      const foundUser = await User.findById(userOneId)
-      expect(foundUser._id).toEqual(userOneId)
-      expect(foundUser.name).toEqual(userOne.name)
-      expect(foundUser.email).not.toEqual(update.email)
-    })
-
-    it('should NOT update a user with an invalid name', async () => {
-      
-      const update = {
-        email: 'user@example.net',
-        password: 'asfdASDF1234!@#$',
-        name: 'a'
-      }
-
-      await request(app)
-        .patch(`/users/${ userOneId }`)
-        .set('Authorization', `Bearer ${ userZero.tokens[0].token }`)
-        .send(update)
-        .expect(400)
-
-      const foundUser = await User.findById(userOneId)
-      expect(foundUser._id).toEqual(userOneId)
-      expect(foundUser.name).toEqual(userOne.name)
-      expect(foundUser.email).not.toEqual(update.email)
-    })
 
     it('should NOT update a user if the email is in use by another user', async () => {
       
